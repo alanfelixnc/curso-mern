d72a03833c98df49bc821f225c91d5a35bbdbe05
Alan Felix <felixnascc@gmail.com>
2021-10-30 15:12:24 -0300

    Conexão com o MongoDB (local)

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.port || 5000;
 
+mongoose.connect(
+  "mongodb://localhost:27017/curso-basico-mern",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully connected to MongoDB!");
+    }
+  }
+);
+
 app.use(cors());
-
 app.use(cookieParser());
-
 app.use(express.json());
 
 app.get("/home", function (req, res) {
