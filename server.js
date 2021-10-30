ede4d8cd84a86c22d00b644e4737525a8af00297
Alan Felix <felixnascc@gmail.com>
2021-10-30 13:32:46 -0300

    Inicialização do servidor web

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Ohayo Sekai");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.get("/home", function (req, res) {
+  res.json({ message: "Ohayo Sekai" });
+});
+
+app.listen(port, function () {
+  console.log(`Server running on port ${port}`);
+});
